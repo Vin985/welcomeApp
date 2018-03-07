2c2b82aeb7c7a023e3885ed19d3d8b284c5302a8
Vin985 <sylvain.christin@gmail.com>
2018-03-07 13:45:21 -0400

    test

--- server.R
+++ server.R
@@ -1,5 +1,5 @@
 # This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
+# You c an find out more about building applications with Shiny here:
 #
 # http://shiny.rstudio.com
 #
